6c62ae71ddf112dbaf540d20cc7a29f9b021f0ee
Hiram Epifânio <hiram.epifanio@gmail.com>
2021-11-08 12:33:29 -0300

    Login and logout features implemented

---
+++ src/main/java/br/com/ghsoftware/veetask/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package br.com.ghsoftware.veetask;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin(form -> form
+				.loginPage("/login")
+				.defaultSuccessUrl("/user/tasks", true)
+				.permitAll())
+			.logout()
+				.permitAll();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		UserDetails user = User.builder()
+				.username("gabi")
+				.password(encoder.encode("gabi"))
+				.roles("ADM")
+				.build();
+		
+		auth.jdbcAuthentication()
+				.dataSource(dataSource)
+				.passwordEncoder(encoder)
+				.withUser(user);
+	}
+}
--- src/main/java/br/com/ghsoftware/veetask/model/User.java
+++ src/main/java/br/com/ghsoftware/veetask/model/User.java
@@ -3,14 +3,12 @@ package br.com.ghsoftware.veetask.model;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
 
 	@Id
